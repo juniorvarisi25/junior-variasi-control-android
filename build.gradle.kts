1833e86010a90f83337e962d04a2b590bb7b9226
juniorvarisi25 <variasijunior@gmail.com>
2026-09-12 19:04:24 +0700

    Fix root Gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,3 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("com.android.application") version "8.7.3" apply false
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-rootProject.name = "JuniorVariasiControl"
-include(":app")
